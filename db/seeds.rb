ecdcc2a16a0eb7fac21388e77d64ac0880048a7d
Felippe Menezes <felippemenezes1982@gmail.com>
2025-11-10 22:10:26 -0300

    Basic back-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'net/http'
+require 'json'
+require 'uri'
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+puts "Database cleaned."
+
+puts "Fetching top rated movies from TMDB..."
+
+url = URI("https://tmdb.lewagon.com/movie/top_rated")
+
+response_string = Net::HTTP.get(url)
+response_hash = JSON.parse(response_string)
+
+movies = response_hash['results']
+
+puts "Creating 10 movies from API..."
+
+movies.first(10).each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+  puts "Created #{movie['title']}"
+end
+
+puts "Finished! Created #{Movie.count} movies."
+
+puts "Creating lists..."
+list_names = ["Drama", "Comedy", "Classic", "To rewatch"]
+list_names.each do |name|
+  List.create!(name: name)
+  puts "Created list: #{name}"
+end
+
+
